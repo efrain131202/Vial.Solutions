0707624530e62814465de2802608d5bd3e8de9c4
joseph cruz galaviz <josephcruzgalaviz20@gmail.com>
2023-12-01 23:06:15 -0600

    agregando boton flofante

--- script.js
+++ script.js
@@ -1,55 +1,52 @@
-  //
+document.addEventListener("DOMContentLoaded", function() {
+  const navbarLinks = document.querySelectorAll(".navbar-items a");
+  const toggleButton = document.getElementById("toggleButton");
+  const navbar = document.querySelector('.navbar');
+  const goToTopButton = document.getElementById("goToTopBtn");
+
+  navbarLinks.forEach(link => {
+    link.addEventListener("click", smoothScroll);
+  });
+
+  toggleButton.addEventListener('click', function() {
+    navbar.classList.toggle('active');
+  });
+
+  function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    const navbarHeight = document.querySelector('.navbar').offsetHeight;
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - navbarHeight,
+        behavior: "smooth"
+      });
+      navbar.classList.remove('active');
+    }
+  }
+
+  goToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+});
 
 document.addEventListener("DOMContentLoaded", function() {
-    const navbarLinks = document.querySelectorAll(".navbar-items a");
-    const toggleButton = document.getElementById("toggleButton");
-    const navbar = document.querySelector('.navbar');
-  
-    navbarLinks.forEach(link => {
-      link.addEventListener("click", smoothScroll);
-    });
-  
-    toggleButton.addEventListener('click', function() {
-      navbar.classList.toggle('active');
-    });
+  const text = document.querySelector('.text');
+  text.style.animationPlayState = 'running';
+});
 
-  //
-  
-    function smoothScroll(event) {
-      event.preventDefault();
-      const targetId = this.getAttribute("href");
-      const targetElement = document.querySelector(targetId);
-      const navbarHeight = document.querySelector('.navbar').offsetHeight;
-  
-      if (targetElement) {
-        window.scrollTo({
-          top: targetElement.offsetTop - navbarHeight,
-          behavior: "smooth"
-        });
-        navbar.classList.remove('active');
-      }
-    }
-  });
+window.addEventListener('scroll', () => {
+  const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
 
-  //
-
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    const text = document.querySelector('.text');
-    text.style.animationPlayState = 'running';
-  });
-
-  //
-
-  window.addEventListener('scroll', () => {
-    const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
-  
-    const progressBar = document.querySelector('.progress-bar');
-    progressBar.style.width = scrolled + '%';
-  });
-
-  //
+  const progressBar = document.querySelector('.progress-bar');
+  progressBar.style.width = scrolled + '%';
+});
 
   
